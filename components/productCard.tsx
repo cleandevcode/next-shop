2a02a1c117ebf380f514f51b83310491f3ed547b
cleandevcode <freelance1331@gmail.com>
2021-02-18 17:00:32 +0300

    bug1

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Grid from "@material-ui/core/Grid";
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Link from "next/link";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
@@ -60,14 +58,6 @@ export default function ProductCard({ row, symbol, currency }) {
               </Typography>
             </CardContent>
           </CardActionArea>
-          {/* <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions> */}
         </Card>
         </Link>
       </Grid>
